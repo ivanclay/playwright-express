cd9726c55a37fdbb346f225ec7bec87193701d83
Ivan Clay <ivan.clay.moura@gmail.com>
2023-11-02 11:22:36 -0300

    add lib `faker-js` to generate test mass

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,38 +1,39 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test('deve poder cadastrar uma nova tarefa via id -> Estratégia 01', async ({ page }) => {
     await page.goto('http://localhost:3000');
-    await page.fill('#newTask', 'Ler um livro de TypeScript');
+    await page.fill('#newTask', faker.lorem.words());
 });
 
 test('deve poder cadastrar uma nova tarefa via placeholder -> Estratégia 02', async ({ page }) => {
     await page.goto('http://localhost:3000');
-    await page.fill('input[placeholder="Add a new Task"]', 'Ler um livro de TypeScript');
+    await page.fill('input[placeholder="Add a new Task"]', faker.lorem.words());
 });
 
 test('deve poder cadastrar uma nova tarefa via type -> Estratégia 03', async ({ page }) => {
     await page.goto('http://localhost:3000');
-    await page.fill('input[type=text]', 'Ler um livro de TypeScript');
+    await page.fill('input[type=text]', faker.lorem.words());
 });
 
 test('deve poder cadastrar uma nova tarefa via classe -> Estratégia 04', async ({ page }) => {
     await page.goto('http://localhost:3000');
     //regular expression to find through dinamic classes
-    await page.fill('input[class*=InputNewTask]', 'Ler um livro de TypeScript');
+    await page.fill('input[class*=InputNewTask]', faker.lorem.words());
 });
 
 test('deve poder cadastrar uma nova tarefa usando locator -> Estratégia 05', async ({ page }) => {
     await page.goto('http://localhost:3000');
     
     const inputTaskLocator = page.locator('input[class*=InputNewTask]');
-    await inputTaskLocator.fill('Ler um livro de TypeScript');
+    await inputTaskLocator.fill(faker.lorem.words());
 });
 
 test('deve submeter o formulário teclando enter', async ({ page }) => {
     await page.goto('http://localhost:3000');
     
     const inputTaskLocator = page.locator('input[class*=InputNewTask]');
-    await inputTaskLocator.fill('Ler um livro de TypeScript');
+    await inputTaskLocator.fill(faker.lorem.words());
     await inputTaskLocator.press('Enter');
 });
 
@@ -40,7 +41,7 @@ test('deve submeter o formulário clicando no botão Create -> Estratégia 01', 
     await page.goto('http://localhost:3000');
     
     const inputTaskLocator = page.locator('input[class*=InputNewTask]');
-    await inputTaskLocator.fill('Ler um livro de TypeScript');
+    await inputTaskLocator.fill(faker.lorem.words());
     await page.click('xpath=//button[contains(text(), "Create")]');
 });
 
@@ -48,6 +49,6 @@ test('deve submeter o formulário clicando no botão Create -> Estratégia 02', 
     await page.goto('http://localhost:3000');
     
     const inputTaskLocator = page.locator('input[class*=InputNewTask]');
-    await inputTaskLocator.fill('Ler um livro de TypeScript');
+    await inputTaskLocator.fill(faker.lorem.words());
     await page.click('css=button >> text=Create');
 });
