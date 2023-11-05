3cb3521afc767f0543253511ab37d8e3de88b3a6
Ivan Clay <ivan.clay.moura@gmail.com>
2023-11-05 12:36:47 -0300

    add `dotenv` lib

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -13,7 +13,7 @@ export class TasksPage{
     }
 
     async go(){
-        await this.page.goto('http://localhost:3000');
+        await this.page.goto('/');
     }
 
     async create(task: TaskModel){
